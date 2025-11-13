ee7d94df3a79a8fecc433946dcd241ccdf594794
Learning Student <student@example.com>
2025-11-13 10:00:00 +0700

    13 Nov 2025: JavaScript ประกาศตัวแปร (var, const, let)

---
+++ index.js
@@ -0,0 +1,14 @@
+// 13 Nov 2025: JavaScript ประกาศตัวแปร (var, const, let)
+
+const text = 'himax';
+var number = 5;
+let message = 'Hello World';
+
+console.log(text);
+console.log(number);
+console.log(message);
+
+
+
+
+
